5a651cd0c2ffe3587c79f93e0918184d55404cf8
yosrio <yosrio71@gmail.com>
2023-04-01 14:39:54 +0700

    Create button logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -21,8 +21,8 @@
 		<nav id="sidebar" class="sidebar js-sidebar">
 			<div class="sidebar-content js-simplebar">
 				<a class="sidebar-brand" href="index.html">
-          <span class="align-middle">Backoffice</span>
-        </a>
+					<span class="align-middle">Backoffice</span>
+				</a>
 
 				<ul class="sidebar-nav">
 					<li class="sidebar-header">
@@ -31,8 +31,8 @@
 
 					<li class="sidebar-item active">
 						<a class="sidebar-link" href="/dashboard">
-              <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
-            </a>
+							<i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+						</a>
 					</li>
 				</ul>
 			</div>
@@ -41,16 +41,30 @@
 		<div class="main">
 			<nav class="navbar navbar-expand navbar-light navbar-bg">
 				<a class="sidebar-toggle js-sidebar-toggle">
-                    <i class="hamburger align-self-center"></i>
-                </a>
+					<i class="hamburger align-self-center"></i>
+				</a>
+				<div class="navbar-collapse collapse">
+					<ul class="navbar-nav navbar-align">
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
+								<span class="text-dark"><?php echo $user->name ?></span>
+							</a>
+							<div class="dropdown-menu dropdown-menu-end">
+								<a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="{{ route('logout') }}">Log out</a>
+							</div>
+						</li>
+					</ul>
+				</div>
 			</nav>
 
 			<main class="content">
-                @yield('content')
+				@yield('content')
 			</main>
 
 			<footer class="footer">
-                
+
 			</footer>
 		</div>
 	</div>
