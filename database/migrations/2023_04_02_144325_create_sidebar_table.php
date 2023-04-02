f038eb5f45945b3a833806edf96c95576d900436
yosrio <yosrio71@gmail.com>
2023-04-02 22:15:24 +0700

    Dynamic Menu - Create Table Sidebar

---
+++ database/migrations/2023_04_02_144325_create_sidebar_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sidebar', function (Blueprint $table) {
+            $table->id();
+            $table->string('header', 255);
+            $table->text('item');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sidebar');
+    }
+};
