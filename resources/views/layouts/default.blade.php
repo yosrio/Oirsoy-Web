e0662350b72083ddf438e76a6f3edbc082eff177
yosrio <yosrio71@gmail.com>
2023-04-02 15:12:03 +0700

    Adjsut some styling

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -14,6 +14,8 @@
 
 	<link href="{{ asset('admin/dist/css/app.css') }}" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
@@ -21,18 +23,29 @@
 	<div class="wrapper">
 		<nav id="sidebar" class="sidebar js-sidebar">
 			<div class="sidebar-content js-simplebar">
-				<a class="sidebar-brand" href="index.html">
+				<a class="sidebar-brand" href="/">
 					<span class="align-middle">Backoffice</span>
 				</a>
-
-				<ul class="sidebar-nav">
+				<ul class="sidebar-nav" id="sidebarMenu">
 					<li class="sidebar-header">
 						Pages
 					</li>
-
-					<li class="sidebar-item active">
-						<a class="sidebar-link" href="/dashboard">
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="{{ route('dashboard') }}" id="dashboardMenu" name="dashboardMenu">
 							<i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+						</a>
+					</li>
+					<li class="sidebar-header">
+						Admin
+					</li>
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="{{ route('users') }}" id="usersMenu" name="usersMenu">
+							<i class="align-middle" data-feather="users"></i> <span class="align-middle">Users</span>
+						</a>
+					</li>
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="" id="rolesMenu" name="rolesMenu">
+							<i class="align-middle" data-feather="user-check"></i> <span class="align-middle">Roles</span>
 						</a>
 					</li>
 				</ul>
@@ -41,6 +54,7 @@
 
 		<div class="main">
 			<nav class="navbar navbar-expand navbar-light navbar-bg">
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 				<a class="sidebar-toggle js-sidebar-toggle">
 					<i class="hamburger align-self-center"></i>
 				</a>
