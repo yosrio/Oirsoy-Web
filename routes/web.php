26d418b3457a808afe4decdba5c6a780200aa087
yosrio <yosrio71@gmail.com>
2023-04-05 20:25:38 +0700

    Update route url

--- routes/web.php
+++ routes/web.php
@@ -17,25 +17,25 @@ use App\Http\Controllers\RolesController;
 |
 */
 
-Route::get('/', [AuthController::class, 'index'])->name('login');
-Route::get('dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
-Route::get('login', [AuthController::class, 'index'])->name('login');
-Route::post('login', [AuthController::class, 'customLogin'])->name('login.custom');
-Route::get('register', [AuthController::class, 'register'])->name('register');
-Route::post('register', [AuthController::class, 'customRegister'])->name('register.custom');
-Route::get('logout', [AuthController::class, 'logOut'])->name('logout');
+Route::get('admin/', [AuthController::class, 'index'])->name('login');
+Route::get('admin/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+Route::get('admin/login', [AuthController::class, 'index'])->name('login');
+Route::post('admin/login', [AuthController::class, 'customLogin'])->name('login.custom');
+Route::get('admin/register', [AuthController::class, 'register'])->name('register');
+Route::post('admin/register', [AuthController::class, 'customRegister'])->name('register.custom');
+Route::get('admin/logout', [AuthController::class, 'logOut'])->name('logout');
 
-Route::get('profile', [ProfileController::class, 'index'])->name('profile');
-Route::post('profile/updateProfile', [ProfileController::class, 'updateProfile'])->name('updateProfile');
+Route::get('admin/profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('admin/profile/updateProfile', [ProfileController::class, 'updateProfile'])->name('updateProfile');
 
-Route::get('users', [UserController::class, 'index'])->name('users');
-Route::get('users/add', [UserController::class, 'addOrUpdate'])->name('user.add');
-Route::post('users', [UserController::class, 'save'])->name('user.save');
-Route::get('users/update/{id}', [UserController::class, 'addOrUpdate'])->name('user.update');
-Route::get('users/{id}', [UserController::class, 'delete'])->name('user.delete');
+Route::get('admin/users', [UserController::class, 'index'])->name('users');
+Route::get('admin/users/add', [UserController::class, 'addOrUpdate'])->name('user.add');
+Route::post('admin/users', [UserController::class, 'save'])->name('user.save');
+Route::get('admin/users/update/{id}', [UserController::class, 'addOrUpdate'])->name('user.update');
+Route::get('admin/users/{id}', [UserController::class, 'delete'])->name('user.delete');
 
-Route::get('roles', [RolesController::class, 'index'])->name('roles');
-Route::get('roles/add', [RolesController::class, 'addOrUpdate'])->name('roles.add');
-Route::post('roles', [RolesController::class, 'save'])->name('roles.save');
-Route::get('roles/update/{id}', [RolesController::class, 'addOrUpdate'])->name('roles.update');
-Route::get('roles/{id}', [RolesController::class, 'delete'])->name('roles.delete');
+Route::get('admin/roles', [RolesController::class, 'index'])->name('roles');
+Route::get('admin/roles/add', [RolesController::class, 'addOrUpdate'])->name('roles.add');
+Route::post('admin/roles', [RolesController::class, 'save'])->name('roles.save');
+Route::get('admin/roles/update/{id}', [RolesController::class, 'addOrUpdate'])->name('roles.update');
+Route::get('admin/roles/{id}', [RolesController::class, 'delete'])->name('roles.delete');
